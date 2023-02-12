1419f977ca0516e987f23b6e76a4b37d05e955bb
Marcos Andrade <mvpdeandrade@gmail.com>
2023-02-12 10:38:37 -0300

    Cria função que compara os valores do array com o minimo da função

--- 7. Getting The Min Value Using Params/Program.cs
+++ 7. Getting The Min Value Using Params/Program.cs
@@ -6,7 +6,21 @@ namespace _7._Getting_The_Min_Value_Using_Params
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int min = MinV2(6, 4, 2, 8, 0, 1, 5); //7 valores
+                                                  //Math.Min(3 , 2.5); Output: 2.5; -----> math.min funciona com int vs decimal
+            Console.WriteLine("athe minimun is {0} : ", min);                                                  
+        }
+        public static int MinV2(params int[] numbers)
+        {
+            int min = int.MaxValue; //Usando o MaxValue, agora eu posso comparar o numero que eu vou passar com o mínimo
+            foreach (int number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            return min;
         }
     }
 }
