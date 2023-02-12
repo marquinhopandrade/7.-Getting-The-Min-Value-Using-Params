6fa062831af9c210a874b918ce2cdce229f7bb77
Marcos Andrade <mvpdeandrade@gmail.com>
2023-02-12 10:27:22 -0300

    Adicionar arquivos de projeto.

---
+++ 7. Getting The Min Value Using Params/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7._Getting_The_Min_Value_Using_Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
